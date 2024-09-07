eb8a29711129e0b29a41c503eca8c1b3c9af93c3
ShubhamKrishna17 <blograthore@gmail.com>
2024-09-07 17:23:27 +0000

    Maths and Number

---
+++ basics/num_maths.js
@@ -0,0 +1,30 @@
+let score = 100;
+// console.log(score);
+
+let newScore = new Number(200000000000);
+// console.log(newScore.toFixed(3));
+// console.log(newScore.toPrecision(1));
+// console.log(newScore.toLocaleString());
+
+
+
+
+/*  MATHS   */
+
+// console.log(Math.abs(-10));
+// console.log(Math.round(10.3456));
+// console.log(Math.ceil(20.34));
+// console.log(Math.sqrt(25));
+// console.log(Math.pow(4,2));
+// console.log(Math.floor(4.657));
+// console.log(Math.max(4,2,34,76));
+// console.log(Math.min(4,2,34,76));
+
+console.log(Math.ceil((Math.random()*5)) + 1);
+
+let max= 15;
+let min = 5;
+console.log(Math.ceil(Math.random()*(max - min)) + min);
+
+
+
