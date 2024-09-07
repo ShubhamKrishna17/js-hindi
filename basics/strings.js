b136de2c6f666ffcd8c09d2b9bec79321f25b83f
ShubhamKrishna17 <blograthore@gmail.com>
2024-09-07 16:24:03 +0000

    strings

---
+++ basics/strings.js
@@ -0,0 +1,35 @@
+// Strings bacics
+
+let naam = "           shu           bham         "
+
+let newName = new String('Rohit');
+
+// console.log("My name is " + "Shubham");
+// console.log(`My name is ${naam} Rohit`);
+
+// console.log(naam);
+// console.log(newName.toUpperCase());
+// console.log(newName.__proto__);
+// console.log(newName.charAt(3));
+// console.log(newName.indexOf('t'));
+
+// console.log(newName[2]);
+
+
+let nameTest = naam.substring(0,7);
+//console.log(nameTest);
+
+
+let updatedName = naam.slice(0,5);
+//console.log(updatedName);
+
+// console.log(naam.trim()); trims whitespace
+
+let url = "shubhamrathore@google.com";
+
+// console.log(url.replace('google','gmail'));
+
+let str = "Shubham Singh Rathore";
+console.log(str.split(' ')); // converts to array
+
+
