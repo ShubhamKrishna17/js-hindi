018c1313ab0bdccbe5ff553613d572fd329ebf85
ShubhamKrishna17 <blograthore@gmail.com>
2024-09-08 05:03:40 +0000

    Arrays basics

---
+++ basics2/arrays.js
@@ -0,0 +1,38 @@
+// ARRAYS
+
+let myArr = [0,1,2,3,4,5];
+
+let myArr2 = ["Shubham","Rohit","Krishna"];
+let myArr4 = myArr2.join();
+
+// console.log(typeof myArr2);
+// console.log(typeof myArr4);
+
+let myArr3 = new Array(5,6,7,7);
+// console.log(myArr,myArr2, myArr3);
+
+
+// ARRAY METHODS
+
+myArr.push(10,20,30,40);
+myArr2.pop();
+
+
+// console.log(myArr2);
+
+// myArr.unshift(5);
+
+// myArr2.shift('Krishna');
+// console.log(myArr);
+// console.log(myArr2.includes('Shubham'));
+
+// Slice and SPLICE
+
+
+console.log(myArr.slice(1,4));
+console.log(myArr);
+console.log(myArr.splice(1,4));
+console.log(myArr);
+
+
+
