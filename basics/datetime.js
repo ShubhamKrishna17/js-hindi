fda21d909c3e2700f5041e100a1944a6bd24b92e
ShubhamKrishna17 <blograthore@gmail.com>
2024-09-08 04:33:04 +0000

    Date and Time

---
+++ basics/datetime.js
@@ -0,0 +1,25 @@
+// Dates
+
+let mydate = new Date(2023, 4, 25, 28, 12);
+
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toLocaleTimeString());
+// console.log(mydate.toJSON());
+
+// console.log(typeof mydate);
+
+// console.log(mydate.toLocaleString());
+
+let mytime = new Date(2024,3,23);
+// console.log(mytime.getTime()/1000);
+
+// console.log(Math.floor(Date.now()/1000));
+
+let day = new Date();
+console.log(day.getDate());
+console.log(day.getFullYear());
+console.log(day.getMinutes());
+console.log(day.getMonth());
+
